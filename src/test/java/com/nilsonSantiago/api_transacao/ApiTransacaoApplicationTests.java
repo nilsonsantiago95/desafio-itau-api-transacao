40779cdc65b26c80f45420eac68084ca9d24a635
Nilson Santiago <nilson.santiago19@gmail.com>
2026-08-01 08:32:22 -0300

    first commit

---
+++ src/test/java/com/nilsonSantiago/api_transacao/ApiTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilsonSantiago.api_transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
